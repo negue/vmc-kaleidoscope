74521697642ad0a7a27be79fa39c56cc9f8f9d7a
Dominik Nakamura <dnaka91@gmail.com>
2022-07-11 11:50:30 +0900

    Log endlessly until shutdown signal received

--- osc-dump/dump_osc_vmc_data.go
+++ osc-dump/dump_osc_vmc_data.go
@@ -10,52 +10,102 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
+	// Open UDP listener connection.
 	conn, err := net.ListenPacket("udp", ":39543")
 	if err != nil {
 		log.Fatal("failed listening on UDP", err)
 	}
 
+	// Create output file, which is GZIP-compressed and swapped automatically, once it reaches
+	// the given size limit (currently 8 MB).
 	out, err := newSwapWriter(8 * 1024 * 1024)
 	if err != nil {
 		log.Fatal("failed creating output file writer", err)
 	}
-	defer out.Close()
 
-	start := time.Now()
+	// Install a shutdown handler so we can cleanly shut down and close our output file properly.
+	// Also, allocate some buffers for reading/saving UDP data.
+	shutdown := installShutdownHandler()
 	sizeBuf := make([]byte, 2)
 	dataBuf := make([]byte, 65535)
 
-	for time.Since(start) < 5*time.Minute {
-		if err := conn.SetReadDeadline(time.Now().Add(10 * time.Second)); err != nil {
-			log.Fatal("failed setting read deadline", err)
-		}
-
-		n, _, err := conn.ReadFrom(dataBuf)
-		if errors.Is(err, os.ErrDeadlineExceeded) {
-			log.Printf("no data within the last 10 seconds, are we connected?")
-			continue
-		}
-		if err != nil {
-			log.Fatal("failed reading from UDP")
-		}
-
-		binary.BigEndian.PutUint16(sizeBuf, uint16(n))
-
-		if _, err := out.Write(sizeBuf); err != nil {
-			log.Fatal("failed writing package size", err)
-		}
-		if _, err := out.Write(dataBuf[:n]); err != nil {
-			log.Fatal("failed writing package data", err)
+loop:
+	// Main loop, reading data endlessly until we receive a shutdown signal.
+	for {
+		select {
+		// Got a shutdown signal, lets break out of the read loop.
+		case <-shutdown:
+			break loop
+		// Try to read a new UDP packet, stopping the loop if we failed.
+		default:
+			if err := logData(conn, out, sizeBuf, dataBuf); err != nil {
+				log.Println("failed logging data", err)
+				break loop
+			}
 		}
 	}
 
+	// Close the output file first, so it doesn't turn out as a corrupt GZIP file.
 	if err := out.Close(); err != nil {
 		log.Fatal("failed closing output file writer", err)
 	}
+
+	// Then close the UDP connection.
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed closing UDP connection", err)
+	}
+}
+
+func installShutdownHandler() <-chan struct{} {
+	c := make(chan os.Signal, 1)
+	n := make(chan struct{}, 1)
+
+	// Install OS signal handlers
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	// Spawn a goroutine that will wait for the OS signal and
+	// then notify back on the channel n.
+	go func() {
+		<-c
+		log.Println("shutting down, please wait...")
+		n <- struct{}{}
+		close(n)
+	}()
+
+	return n
+}
+
+func logData(conn net.PacketConn, out io.Writer, dataBuf, sizeBuf []byte) error {
+	// Set a deadline, so we don't block forever if no data comes in at all.
+	if err := conn.SetReadDeadline(time.Now().Add(5 * time.Second)); err != nil {
+		return fmt.Errorf("failed setting read deadline: %w", err)
+	}
+
+	n, _, err := conn.ReadFrom(dataBuf)
+	if errors.Is(err, os.ErrDeadlineExceeded) {
+		log.Println("no data within the last 5 seconds, are we connected?")
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("failed reading from UDP: %w", err)
+	}
+
+	binary.BigEndian.PutUint16(sizeBuf, uint16(n))
+
+	if _, err := out.Write(sizeBuf); err != nil {
+		return fmt.Errorf("failed writing package size: %w", err)
+	}
+	if _, err := out.Write(dataBuf[:n]); err != nil {
+		return fmt.Errorf("failed writing package data: %w", err)
+	}
+
+	return nil
 }
 
 type swapWriter struct {
